37e730ee05582a02ec4ee4108ace5694d1eb77aa
DESKTOP-LFEJR4C\Uriy <lonsdale341@gmail.com>
2017-07-25 20:08:23 +0300

    Init

---
+++ Assets/Sripts/CommonData.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+
+
+  // Container class for constants, statics, and other data
+  // that needs to be cached or shared between different components
+  // or states.
+  class CommonData {
+    public static PrefabList prefabs;
+   
+
+    // Paths to various database tables:
+    // Trailing slashes required, because in some cases
+    // we append further paths onto these.
+   
+
+    // X-Z plane at height 0
+   
+
+   
+
+    // TestLabManager to track the state of the tests coming in from the intent
+   
+
+    // Whether we're signed in or not.
+   
+
+    // Utility function to see if we should be showing menu options that
+    // require internet access and a user profile.
+   
+  }
+
